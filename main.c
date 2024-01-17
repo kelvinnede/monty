d5541f47f32d4279f8d02604d7b488596fb05b1f
Kelvin Nede <kelvinnede@gmail.com>
2024-01-17 00:59:32 -0800

    main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * main - Entry point of the Monty interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s file\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	FILE *file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+
+	stack_t *stack = NULL;
+
+	int result = execute_monty_file(file, &stack);
+
+	fclose(file);
+
+	free_stack(&stack);
+
+	return (result);
+}
