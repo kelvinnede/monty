7577c57023804b262562fee5dc1ec67f6b040520
Kelvin Nede <kelvinnede@gmail.com>
2024-01-17 00:43:18 -0800

    monty.h

---
+++ monty.h
@@ -0,0 +1,23 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
+ */
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+void push(stack_t **stack, int value);
+void pall(stack_t **stack);
+
+#endif /* MONTY_H */
